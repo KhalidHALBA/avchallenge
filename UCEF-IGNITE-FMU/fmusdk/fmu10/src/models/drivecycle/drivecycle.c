cd5e24cacb0282bcd8c78a39bfe52c0179d0f569
KHALID HALBA <khalid.halba@nist.gov>
2019-09-11 23:32:45 -0400

    fmu-fix2

--- UCEF-IGNITE-FMU/fmusdk/fmu10/src/models/drivecycle/drivecycle.c
+++ UCEF-IGNITE-FMU/fmusdk/fmu10/src/models/drivecycle/drivecycle.c
@@ -6,20 +6,21 @@
 #define NUMBER_OF_STRINGS 0
 #define NUMBER_OF_STATES 0
 #define NUMBER_OF_EVENT_INDICATORS 0
-#define BUFLEN 200	//Max length of buffer
+#define BUFLEN 200 //Max length of buffer
 #define BUFLEN1 100
+#define SIMTIME 400
 #define PORT 8888
 #include "fmuTemplate.h"
 #include <winsock2.h>
 #include <wS2tcpip.h>
-#include <stdio.h> 
+#include <stdio.h>
 #include <windows.h>
 
-#pragma comment( lib, "ws2_32.lib")
-int xx = 0;
+#pragma comment(lib, "ws2_32.lib")
 
-#include <stdlib.h> 
-#include <string.h> 
+
+#include <stdlib.h>
+#include <string.h>
 #define cycle_speed_kmph_ 0
 #define cycle_time_ahead_ 1
 #define cycle_time_ 2
@@ -28,159 +29,135 @@ int xx = 0;
 #define driver_brake_demand_ 5
 #define counter_ 6
 
-void setStartValues(ModelInstance *comp) {
-    r(cycle_speed_kmph_) = 0;
-    r(cycle_time_) = 0;
-    r(cycle_speed_ahead_kmph_) = 0;
-    r(cycle_time_ahead_) = 0;
+void setStartValues(ModelInstance *comp)
+{
+	r(cycle_speed_kmph_) = 0;
+	r(cycle_time_) = 0;
+	r(cycle_speed_ahead_kmph_) = 0;
+	r(cycle_time_ahead_) = 0;
 	r(vehicle_velocity_) = 0;
 	r(driver_brake_demand_) = 0;
-    i(counter_) = 0;
+	i(counter_) = 0;
 }
-fmiReal getReal(ModelInstance* comp, fmiValueReference vr){
-    switch (vr) {
-        case cycle_speed_kmph_ : return r(cycle_speed_kmph_);
-        case cycle_time_ : return r(cycle_time_);
-        case cycle_speed_ahead_kmph_ : return r(cycle_speed_ahead_kmph_);
-        case cycle_time_ahead_ : return r(cycle_time_ahead_);
-		case vehicle_velocity_ : return r(vehicle_velocity_);
-		case driver_brake_demand_ : return r(driver_brake_demand_);
-        default: return 0;
-    }
+fmiReal getReal(ModelInstance *comp, fmiValueReference vr)
+{
+	switch (vr)
+	{
+	case cycle_speed_kmph_:
+		return r(cycle_speed_kmph_);
+	case cycle_time_:
+		return r(cycle_time_);
+	case cycle_speed_ahead_kmph_:
+		return r(cycle_speed_ahead_kmph_);
+	case cycle_time_ahead_:
+		return r(cycle_time_ahead_);
+	case vehicle_velocity_:
+		return r(vehicle_velocity_);
+	case driver_brake_demand_:
+		return r(driver_brake_demand_);
+	default:
+		return 0;
+	}
 }
-void initialize(ModelInstance* comp, fmiEventInfo* eventInfo) {
-    eventInfo->upcomingTimeEvent   = fmiTrue;
-    eventInfo->nextEventTime       = 1 + comp->time;
+void initialize(ModelInstance *comp, fmiEventInfo *eventInfo)
+{
+	eventInfo->upcomingTimeEvent = fmiTrue;
+	eventInfo->nextEventTime = 1 + comp->time;
 }
-void eventUpdate(ModelInstance* comp, fmiEventInfo* eventInfo) {
+void eventUpdate(ModelInstance *comp, fmiEventInfo *eventInfo)
+{
 
-
-SOCKET s;
+	SOCKET s;
 	struct sockaddr_in server, si_other;
-	int slen , recv_len;
+	int slen, recv_len;
 	char buf[BUFLEN1];
 	char bufr[BUFLEN];
 	char bufra[BUFLEN1];
 	char bufrb[BUFLEN1];
 	WSADATA wsa;
 
-	slen = sizeof(si_other) ;
-	
+	slen = sizeof(si_other);
+
 	//Initialise winsock
 	// printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
 		// printf("Failed. Error Code : %d",WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 	// printf("Initialised.\n");
-	
+
 	//Create a socket
-	if((s = socket(AF_INET , SOCK_DGRAM , 0 )) == INVALID_SOCKET)
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
 		// printf("Could not create socket : %d" , WSAGetLastError());
 	}
 	// printf("Socket created.\n");
-	
+
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( PORT );
-
-
+	server.sin_port = htons(PORT);
 
 	//Bind
-	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
 	{
 		// printf("Bind failed with error code : %d" , WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
 	puts("Bind done");
 
-
-
-
 	//keep listening for data
-    // try while(counter<value)
+	// try while(counter<value)
 
+	i(counter_) += 1;
 
-    i(counter_) += 1;
-
-    if (i(counter_) > 2000) 
-        eventInfo->terminateSimulation = fmiTrue;
-    else {
-
-
-        eventInfo->upcomingTimeEvent   = fmiTrue;
-        eventInfo->nextEventTime       = 1 + comp->time;
-    
-
-
-
-
-
-
-
-
-	while(1)
+	if (i(counter_) > SIMTIME)
+		eventInfo->terminateSimulation = fmiTrue;
+	else
 	{
 
-		fflush(stdout);
-		
+		eventInfo->upcomingTimeEvent = fmiTrue;
+		eventInfo->nextEventTime = 1 + comp->time;
 
-		memset(buf,0, BUFLEN1);
-		
-		memset(bufr,0, BUFLEN);
-
-		memset(bufra,0, BUFLEN1);
-        memset(bufrb,0, BUFLEN1);
-		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
+		while (SIMTIME)
 		{
-			exit(EXIT_FAILURE);
+
+			fflush(stdout);
+			memset(buf, 0, BUFLEN1);
+			memset(bufr, 0, BUFLEN);
+			memset(bufra, 0, BUFLEN1);
+			memset(bufrb, 0, BUFLEN1);
+
+			if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)) == SOCKET_ERROR)
+			{
+				exit(EXIT_FAILURE);
+			}
+
+			r(cycle_speed_kmph_) = (float)(atoi(buf));
+			int a = ((r(vehicle_velocity_)) * 3.6);
+			int b = 100 * r(driver_brake_demand_);
+
+			sprintf(bufra, "%d", a);
+			sprintf(bufrb, "%d", b);
+
+			strcat(bufr, bufra);
+			strcat(bufr, ".");
+			strcat(bufr, bufrb);
+
+			printf("DriveCycle %f VehicleResponse %f  braking  %d CycleTime %f concatenated and absoluuted %s \n", r(cycle_speed_kmph_), (((r(vehicle_velocity_)) / 1000) * (3600)), b, r(cycle_time_), bufr);
+
+			if (sendto(s, bufr, recv_len, 0, (struct sockaddr *)&si_other, slen) == SOCKET_ERROR)
+			{
+				exit(EXIT_FAILURE);
+			}
+
+			break;
 		}
-		
-        r(cycle_speed_kmph_)=(float)( atoi(buf));
-		// r(cycle_speed_ahead_kmph_)=(float)( atoi(buf) + (rand()%5) );
-
-		//  printf("DriveCycle %f VehicleResponse %f CycleTime %f\n" ,  r(cycle_speed_kmph_), (((r(vehicle_velocity_))/1000)*(3600)), r(cycle_time_));
-
-int a=((r(vehicle_velocity_))*3.6);
-  
-   // 4 digits after the decimal point 
-//    a = floor(10*a)/10; 
-
-int b0=r(driver_brake_demand_);
-
-
-int b= 100*b0 ;
-
-
-
-
-sprintf(bufra, "%d", a);
-sprintf(bufrb, "%d", b);
-
-strcat(bufr, bufra);
-strcat(bufr, ".");
-strcat(bufr, bufrb);
-
- printf("DriveCycle %f VehicleResponse %f  braking  %f CycleTime %f concatenated and absoluuted %s \n" ,  r(cycle_speed_kmph_), (((r(vehicle_velocity_))/1000)*(3600)), b,  r(cycle_time_), bufr);
-
-		if (sendto(s, bufr, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
-		{
-			exit(EXIT_FAILURE);
-		}
-	
-    break;
-
-//try without break
-
-    }
-}
+	}
 	closesocket(s);
 	WSACleanup();
-	
-	return;
 
- }
+	return;
+}
 #include "fmuTemplate.c"
