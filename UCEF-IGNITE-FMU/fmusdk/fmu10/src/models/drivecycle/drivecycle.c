da24c3394a04ad03cbcda1473d64ba500427c94a
KHALID HALBA <khalid.halba@nist.gov>
2019-09-20 04:50:33 -0400

    sendin-rate_fixed

--- UCEF-IGNITE-FMU/fmusdk/fmu10/src/models/drivecycle/drivecycle.c
+++ UCEF-IGNITE-FMU/fmusdk/fmu10/src/models/drivecycle/drivecycle.c
@@ -107,10 +107,8 @@ void eventUpdate(ModelInstance *comp, fmiEventInfo *eventInfo)
 	// puts("Bind done");
 	i(counter_) += 1;
 
-
-
 		eventInfo->upcomingTimeEvent = fmiTrue;
-		eventInfo->nextEventTime = 1 + comp->time;
+		eventInfo->nextEventTime = 0.1 + comp->time;
 
 		fflush(stdout);
 		memset(buf, 0, BUFLEN);
@@ -121,34 +119,60 @@ void eventUpdate(ModelInstance *comp, fmiEventInfo *eventInfo)
 		}
 
 		r(cycle_speed_kmph_) = (float)(atoi(buf));
-		int a = 10 * ((r(vehicle_velocity_)) * 3.6);
-		int b = 1000 * r(driver_brake_demand_);
+
+		
+		int a = 100 * ((r(vehicle_velocity_)) * 3.6);
+		int b = 10000 * r(driver_brake_demand_);
+		int t = 100*(r(cycle_time_));
+
 
 		// extract speed decimal values
 
-		int aa1 = a / 100;
-		int aa2 = ((a - (aa1 * 100)) / 10);
-		int aa3 = a - (aa1 * 100) - (aa2 * 10);
+		int aa1 = a / 1000;
+		int aa2 = ((a - (aa1 * 1000)) / 100);
+		int aa3 = ((a - (aa1 * 1000) - (aa2 * 100))/10);
+		int aa4 = a - (aa1 * 1000) - (aa2 * 100) - (aa3*10);
 
 		// extract braking decimal values
 
-		int bb1 = b / 100;
-		int bb2 = ((b - (bb1 * 100)) / 10);
-		int bb3 = b - (bb1 * 100) - (bb2 * 10);
+		int bb1 = b / 1000;
+		int bb2 = ((b - (bb1 * 1000)) / 100);
+		int bb3 = ((b - (bb1 * 1000) - (bb2 * 100))/10);
+		int bb4 = b - (bb1 * 1000) - (bb2 * 100) - (bb3*10);
+
+
+		// extract CycleTime decimal values
+
+		int tt1 = t / 10000;
+		int tt2 = ((t - (tt1 * 10000)) / 1000);
+		int tt3 = ((t - (tt1 * 10000) - (tt2 * 1000))/100);
+		int tt4 = ((t - (tt1 * 10000) - (tt2 * 1000) - (tt3*100))/10);
+		int tt5 = t - (tt1 * 10000) - (tt2 * 1000) - (tt3*100) - (tt4*10) ;
 
 		// convert to ascii
 
 		char s1 = '0' + aa1;
 		char s2 = '0' + aa2;
 		char s3 = '0' + aa3;
+        char s4 = '0' + aa4;
+        
 
 		char b1 = '0' + bb1;
 		char b2 = '0' + bb2;
 		char b3 = '0' + bb3;
+		char b4 = '0' + bb4;
 
-		printf(" update aa1 %d  aa2 %d aa3 %d bb1 %d bb2 %d bb3 %d \n", aa1, aa2, aa3, bb1, bb2, bb3);
 
-		char DYNAMICS[] = {s1, s2, s3, (char)32, b1, b2, b3};
+		char t1 = '0' + tt1;
+		char t2 = '0' + tt2;
+		char t3 = '0' + tt3;
+		char t4 = '0' + tt4;
+		char t5 = '0' + tt5;
+
+        printf(" update a %d                         b %d                        t %d \n", a, b, t);
+		printf(" update aa1 %d  aa2 %d aa3 %d aa4 %d bb1 %d bb2 %d bb3 %d bb4 %d tt1 %d tt2 %d tt3 %d tt4 %d tt5 %d \n", aa1, aa2, aa3, aa4, bb1, bb2, bb3, bb4, tt1, tt2, tt3, tt4, tt5);
+
+		char DYNAMICS[] = {s1, s2, s3, s4, (char)32, b1, b2, b3, b4, (char)32, t1, t2, t3, t4, t5};
 
 		if (sendto(s, DYNAMICS, sizeof(DYNAMICS), 0, (struct sockaddr *)&si_other, slen) == SOCKET_ERROR)
 		{
